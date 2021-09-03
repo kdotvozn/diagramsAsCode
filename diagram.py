fb43ded74c43978d90986aca2de4a31a41556eba
kdotvozn <kdotvozn@gmail.com>
2021-09-03 08:36:17 +0000

    diagram show=False

--- diagram.py
+++ diagram.py
@@ -5,5 +5,5 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram("Web Service", show=True):
+with Diagram("Web Service", show=False):
     ELB("lb") >> EC2("web") >> RDS("userdb")
